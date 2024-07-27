1bfa9f9904d2d54479b154413582fb44a8a15bb8
srijanid <srijanidas06@gmail.com>
2024-07-27 22:26:22 +0530

    password configuration done

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:shopify/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:shopify/features/authentication/screens/signup/signup.dart';
 import 'package:shopify/utils/constants/sizes.dart';
 import 'package:shopify/utils/constants/text_strings.dart';
@@ -46,7 +47,7 @@ class TLoginForm extends StatelessWidget {
                 ),
                 //Forget Password
                 TextButton(
-                    onPressed: () {}, child: const Text(TTexts.forgetPassword)),
+                    onPressed: () => Get.to(() => const ForgetPassword()), child: const Text(TTexts.forgetPassword)),
               ],
             ),
             const SizedBox(height: TSizes.spaceBtwSections),
