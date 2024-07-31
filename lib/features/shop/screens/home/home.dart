363b344fa8e7bfeacca808e1458ba5a242998628
srijanid <srijanidas06@gmail.com>
2024-07-31 19:16:15 +0530

    ClipPath done

---
+++ lib/features/shop/screens/home/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+                child: Column(
+              children: [],
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
